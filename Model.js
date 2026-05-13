b5a492113189ed806bb0688acba40d864a06e135
Barbar Vladyslav <barbar.v.v.-tr51mp@edu.kpi.ua>
2026-05-13 15:13:19 +0300

    refactor: chnage scale of model

--- Model.js
+++ Model.js
@@ -54,7 +54,7 @@ AFRAME.registerComponent('parametric-surface', {
         const wireframeMesh = new THREE.Mesh(geometry, wireframeMaterial);
         mesh.add(wireframeMesh);
 
-        mesh.scale.set(0, 0, 0);
+        mesh.scale.set(0.25, 0.25, 0.25);
         mesh.rotation.x = -Math.PI / 2;
 
         this.el.setObject3D('mesh', mesh);
